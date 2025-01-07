42914ac9b1ac65f6e043eb75403f828d95dbba6c
ElijahInamarga <elijahcek@gmail.com>
2025-01-06 16:40:01 -0800

    Completed basic model of the board

--- Monopoly_Game/Monopoly_Game.cpp
+++ Monopoly_Game/Monopoly_Game.cpp
@@ -2,6 +2,7 @@
 
 Monopoly_Game::Monopoly_Game(QWidget *parent) : QMainWindow(parent), ui(new Ui::Monopoly_GameClass()) {
     ui->setupUi(this);
+    ui->textStart->setText("Start");
 }
 
 Monopoly_Game::~Monopoly_Game() {
